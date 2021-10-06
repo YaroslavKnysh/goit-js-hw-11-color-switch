b9e34c35300ae0936bb929de186cbb3bbaf3f0f8
YaroslavKnysh <yarik.knysh@gmail.com>
2021-10-06 17:46:42 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,33 @@ import colors from './colors.js';
 const buttonStart = document.querySelector('button[data-action="start"]');
 const buttonStop = document.querySelector('button[data-action="stop"]');
 const body = document.querySelector('body');
-console.log(buttonStart);
-console.log(buttonStop);
-console.dir(body);
-console.log(colors);
-
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+// console.log(buttonStart);
+// console.log(buttonStop);
+// console.dir(body);
+// console.log(colors);
 
 buttonStart.addEventListener('click', changeColor);
 buttonStop.addEventListener('click', reset);
 
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+let interval = null;
+
 function changeColor() {
-  body.style.backgroundColor = '#000';
+  interval = setInterval(() => {
+    const min = 0;
+    const max = colors.length - 1;
+    const i = randomIntegerFromInterval(min, max);
+    body.style.backgroundColor = colors[i];
+    buttonStart.setAttribute('disabled', true);
+  }, 500);
 }
 
-function reset(e) {}
+function reset() {
+  const text = 'Вы остановили интервал!)';
+  clearInterval(interval);
+  buttonStart.removeAttribute('disabled');
+  body.style.backgroundColor = colors[0];
+  alert(text);
+}
